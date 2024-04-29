18f12976e44aa671d34dcdbcc595ec73b7c4d403
Elena Cristescu <elenacristescu11@gmail.com>
2024-04-29 15:50:23 +0200

    add color, styling the text and add spacing

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -2,9 +2,9 @@ import { Link } from "react-router-dom";
 
 function CartOverview() {
   return (
-    <div>
-      <p>
-        <span>23 pizzas</span>
+    <div className="p-4 px-4 py-4 text-sm uppercase md:text-base bg-stone-800 text-stone-200 sm:px-6">
+      <p className="space-x-4 font-semibold text-stone-300 sm:space-x-6">
+        <span>23 pizzas </span>
         <span>$23.45</span>
       </p>
       <Link to="/cart">Open cart &rarr;</Link>
---
+++ src/features/user/Username.jsx
@@ -0,0 +1,5 @@
+function Username() {
+  return <div className="hidden text-sm font-semibold">Elena</div>;
+}
+
+export default Username;
