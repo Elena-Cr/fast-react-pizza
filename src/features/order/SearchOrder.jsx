8f3a72f29a399190600340ba7eb05a1260b96554
Elena Cristescu <elenacristescu11@gmail.com>
2024-04-30 14:38:08 +0200

    add styling to the form elements

--- src/features/order/SearchOrder.jsx
+++ src/features/order/SearchOrder.jsx
@@ -16,6 +16,7 @@ function SearchOrder() {
         placeholder="Search order #"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
+        className="px-4 py-2 text-sm transition-all duration-300 bg-yellow-100 rounded-full focus:ring-opacity-50 focus:ring-yellow-500 focus:outline-none focus:ring w-28 sm:w-64 sm:focus:w-72 placeholder:text-stone-400"
       />
     </form>
   );
