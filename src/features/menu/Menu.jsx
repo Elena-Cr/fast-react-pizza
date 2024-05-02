20bed58fcc2b4b68559c4b16f9d7d42276f19234
Elena Cristescu <elenacristescu11@gmail.com>
2024-05-02 16:35:34 +0200

    styling the menu

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -5,9 +5,9 @@ import MenuItem from "./MenuItem";
 
 function Menu() {
   const menu = useLoaderData();
-  console.log(menu);
+
   return (
-    <ul>
+    <ul className="px-2 divide-y divide-stone-200 ">
       {menu.map((pizza) => (
         <MenuItem pizza={pizza} key={pizza.id} />
       ))}
--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,18 +1,22 @@
 import { Link } from "react-router-dom";
 
-function Button({ children, disabled, to }) {
-  const className =
-    "inline-block px-4 py-3 font-semibold tracking-wide uppercase transition-colors duration-300 bg-yellow-400 rounded-full sm:px-6 sm:py-4 disabled:cursor-not-allowed focus:ring-offset-2 focus:outline-none focus:bg-yellow-300 focus:ring-yellow-300 focus:ring hover:bg-yellow-300 text-stone-800";
+function Button({ children, disabled, to, type }) {
+  const base =
+    "inline-block font-semibold tracking-wide uppercase transition-colors duration-300 bg-yellow-400 rounded-full  disabled:cursor-not-allowed focus:ring-offset-2 focus:outline-none focus:bg-yellow-300 focus:ring-yellow-300 focus:ring hover:bg-yellow-300 text-stone-800";
 
+  const styles = {
+    primary: base + " px-4 py-3 md:px-6 md:py-4",
+    small: base + " px-4 py-2 sm:px-5 sm:py-2.5 text-xs",
+  };
   if (to)
     return (
-      <Link className={className} to={to}>
+      <Link className={styles[type]} to={to}>
         {children}
       </Link>
     );
 
   return (
-    <button disabled={disabled} className={className}>
+    <button disabled={disabled} className={styles[type]}>
       {children}
     </button>
   );
