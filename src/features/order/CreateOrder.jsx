1e9c575c1ff436579356606312f6129b05867825
Elena Cristescu <elenacristescu11@gmail.com>
2024-05-03 18:35:12 +0200

    styling the order form and adding the page title and icon

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -44,33 +44,42 @@ function CreateOrder() {
   const cart = fakeCart;
 
   return (
-    <div>
-      <h2>Ready to order? Lets go!</h2>
+    <div className="px-4 py-6">
+      <h2 className="mb-8 text-xl font-semibold">Ready to order? Lets go!</h2>
 
       {/* <Form method="POST" action="/order/new"> */}
       <Form method="POST">
-        <div>
-          <label>First Name</label>
-          <input type="text" name="customer" required className="input" />
+        <div className="flex flex-col gap-2 mb-5 sm:flex-row sm:items-center">
+          <label className="sm:basis-40">First Name</label>
+          <input type="text" name="customer" required className="input grow" />
         </div>
 
-        <div>
-          <label>Phone number</label>
-          <div>
-            <input type="tel" name="phone" required className="input" />
+        <div className="flex flex-col gap-2 mb-5 sm:flex-row sm:items-center">
+          <label className="sm:basis-40">Phone number</label>
+          <div className="grow">
+            <input type="tel" name="phone" required className="w-full input" />
           </div>
           {/* Conditional rendering of the errors */}
-          {formErrors?.phone && <p>{formErrors?.phone}</p>}
+          {formErrors?.phone && (
+            <p className="p-2 mt-2 text-xs text-red-700 bg-red-100 rounded-md">
+              {formErrors?.phone}
+            </p>
+          )}
         </div>
 
-        <div>
-          <label>Address</label>
-          <div>
-            <input type="text" name="address" required className="input" />
+        <div className="flex flex-col gap-2 mb-5 sm:flex-row sm:items-center">
+          <label className="sm:basis-40">Address</label>
+          <div className="grow">
+            <input
+              type="text"
+              name="address"
+              required
+              className="w-full input"
+            />
           </div>
         </div>
 
-        <div>
+        <div className="flex items-center gap-5 mb-12">
           <input
             type="checkbox"
             name="priority"
@@ -79,7 +88,9 @@ function CreateOrder() {
             // value={withPriority}
             // onChange={(e) => setWithPriority(e.target.checked)}
           />
-          <label htmlFor="priority">Want to give your order priority?</label>
+          <label htmlFor="priority" className="font-medium">
+            Want to give your order priority?
+          </label>
         </div>
 
         <div>
--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -2,13 +2,13 @@ import { Link } from "react-router-dom";
 
 function Button({ children, disabled, to, type }) {
   const base =
-    "inline-block font-semibold tracking-wide uppercase transition-colors duration-300 bg-yellow-400 rounded-full  disabled:cursor-not-allowed focus:ring-offset-2 focus:outline-none focus:bg-yellow-300 focus:ring-yellow-300 focus:ring hover:bg-yellow-300 text-stone-800";
+    "inline-block text-sm font-semibold tracking-wide uppercase transition-colors duration-300 bg-yellow-400 rounded-full  disabled:cursor-not-allowed focus:ring-offset-2 focus:outline-none focus:bg-yellow-300 focus:ring-yellow-300 focus:ring hover:bg-yellow-300 text-stone-800";
 
   const styles = {
     primary: base + " px-4 py-3 md:px-6 md:py-4",
     small: base + " px-4 py-2 sm:px-5 sm:py-2.5 text-xs",
     secondary:
-      "inline-block font-semibold tracking-wide uppercase border-2 norder-stone-300 transition-colors duration-300  rounded-full  disabled:cursor-not-allowed focus:ring-offset-2 focus:outline-none focus:bg-stone-300 focus:ring-stone-300 focus:ring hover:bg-stone-300 text-stone-800 px-4 py-2.5 md:px-6 md:py-3.5",
+      "inline-block text-sm  font-semibold tracking-wide uppercase border-2 norder-stone-300 transition-colors duration-300  rounded-full  disabled:cursor-not-allowed focus:ring-offset-2 focus:outline-none focus:bg-stone-300 focus:ring-stone-300 focus:ring hover:bg-stone-300 text-stone-800 px-4 py-2.5 md:px-6 md:py-3.5",
   };
   if (to)
     return (
