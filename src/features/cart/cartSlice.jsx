e1317359ef327aeeeb5eb8f4e4ad8120a9dda428
Elena Cristescu <elenacristescu11@gmail.com>
2024-05-05 14:06:32 +0200

    building the cart page and adding the functionality of clearing the cart

--- src/features/cart/cartSlice.jsx
+++ src/features/cart/cartSlice.jsx
@@ -1,10 +1,10 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  //   cart: [],
-  cart: [
-    { pizzaId: 12, name: "Medi", quantity: 2, unitPrice: 16, totalPrice: 32 },
-  ],
+  cart: [],
+  // cart: [
+  //   { pizzaId: 12, name: "Medi", quantity: 2, unitPrice: 16, totalPrice: 32 },
+  // ],
 };
 
 const cartSlice = createSlice({
@@ -46,7 +46,13 @@ export const {
 } = cartSlice.actions;
 
 export default cartSlice.reducer;
+
+export const getCart = (state) => state.cart.cart;
+
+export const getUser = (state) => state.user.username;
+
 export const getTotalCartQuantity = (state) =>
   state.cart.cart.reduce((sum, item) => sum + item.quantity, 0);
+
 export const getTotalCartPrice = (state) =>
   state.cart.cart.reduce((sum, item) => sum + item.totalPrice, 0);
